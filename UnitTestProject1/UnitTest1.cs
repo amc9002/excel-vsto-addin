a423672a4c4581e2237b4a6f88c8ed6600b5a4d8
amc1988 <9002amc@gmail.com>
2026-02-02 20:19:22 +0100

    Add unit test project and update gitignore

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelAddIn1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
