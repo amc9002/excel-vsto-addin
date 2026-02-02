ba04b157a62117c0850782afbfa357291d51ab84
amc1988 <9002amc@gmail.com>
2026-02-02 18:00:54 +0100

    Initial Excel VSTO add-in with ribbon button

---
+++ ExcelAddIn1/OnClick.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelAddIn1
+{
+    public partial class OnClick
+    {
+        private void OnClick_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+        private void BtnExport_Click(object sender, RibbonControlEventArgs e)
+        {
+            var sheet = (Excel.Worksheet)
+                Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+
+            sheet.Cells[1, 1].Value = "Hello from VSTO";
+        }
+
+    }
+
+}
